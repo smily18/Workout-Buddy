c7aeb6c32d26f18ccf85b51b7d325e3cd7267304
smily18 <smilysaran.18@gmail.com>
2023-03-09 16:13:55 +0530

    First Commit

---
+++ client/src/components/WorkoutForm.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
+
+  const [title, setTitle] = useState("");
+  const [load, setLoad] = useState("");
+  const [reps, setReps] = useState("");
+  const [error, setError] = useState(null);
+  const [empty, setEmpty] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const workout = { title, load, reps };
+
+    const response = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
+      console.log(json.empty);
+      setEmpty(json.empty);
+    }
+    if (response.ok) {
+      setError(null);
+      setTitle("");
+      setLoad("");
+      setReps("");
+      setEmpty([]);
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
+    }
+  };
+
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a New Workout</h3>
+
+      <label>Excersize Title:</label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className={empty.includes('title') ? "error" : " "}
+        
+      />
+
+      <label>Load (in kg):</label>
+      <input
+        type="number"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+        className={empty.includes('load') ? "error" : ""}
+      />
+
+      <label>Number of Reps:</label>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+        className={empty.includes('reps') ? "error" : ""}
+      />
+
+      <button>Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
+export default WorkoutForm;
