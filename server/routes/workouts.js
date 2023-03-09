cdedc72880f1a1a7c06c306349331651b1e12e54
smily18 <smilysaran.18@gmail.com>
2023-03-09 18:25:33 +0530

    CORS Origin2

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -9,6 +9,12 @@ const {
 
 const router = express.Router()
 
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // GET all workouts
 router.get('/', getWorkouts)
 
