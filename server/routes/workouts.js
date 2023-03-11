0bfb65b53c7212bc5dadb427abf7099e8887b586
smily18 <smilysaran.18@gmail.com>
2023-03-11 11:48:20 +0530

    cors npm

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,33 +1,35 @@
-const express = require('express')
+const express = require("express");
+const cors=require("cors");
 const {
-  getWorkouts, 
-  getWorkout, 
-  createWorkout, 
-  deleteWorkout, 
-  updateWorkout
-} = require('../controllers/workoutController')
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
 
-const router = express.Router()
+const router = express.Router();
 
 router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
+router.use(cors());
 
 // GET all workouts
-router.get('/', getWorkouts)
+router.get("/", getWorkouts);
 
 // GET a single workout
-router.get('/:id', getWorkout)
+router.get("/:id", getWorkout);
 
 // POST a new workout
-router.post('/', createWorkout)
+router.post("/", createWorkout);
 
 // DELETE a workout
-router.delete('/:id', deleteWorkout)
+router.delete("/:id", deleteWorkout);
 
 // UPDATE a workout
-router.patch('/:id', updateWorkout)
+router.patch("/:id", updateWorkout);
 
-module.exports = router
+module.exports = router;
